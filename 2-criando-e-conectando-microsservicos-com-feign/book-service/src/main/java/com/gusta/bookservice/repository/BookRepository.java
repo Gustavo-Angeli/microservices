4a0dd2ee7ff3aa9846a24debf7cd9aec9239a5ed
Gustavo-Angeli <gustavoangeli.silva@outlook.com>
2023-02-10 19:43:13 -0300

    ...

--- 2-criando-e-conectando-microsservicos-com-feign/book-service/src/main/java/com/gusta/bookservice/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gusta.bookservice.repository;
-
-import com.gusta.bookservice.model.entity.*;
-import org.springframework.data.jpa.repository.*;
-import org.springframework.stereotype.*;
-
-@Repository
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
